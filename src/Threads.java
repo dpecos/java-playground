ecec0fad1e44e36397cbc2d1987f872a502d2772
Dani Pecos Martinez <dpecosmartinez@bol.com>
2024-06-06 16:40:46 +0200

    Threads example

---
+++ src/Threads.java
@@ -0,0 +1,43 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.Executors;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+// https://howtodoinjava.com/java/multi-threading/virtual-threads/
+public class Threads {
+
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            try {
+                Thread.sleep(Duration.of(1, SECONDS));
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+
+            System.out.println(Thread.currentThread() + " done");
+        };
+
+        // platform threads
+        Instant init = Instant.now();
+        try (var executor = Executors.newFixedThreadPool(100)) {
+            for (var i = 0; i < 10_000; i++) {
+                executor.submit(task);
+            }
+        }
+        Duration ellapsedPlatformThreads = Duration.between(init, Instant.now());
+
+        // virtual threads
+        init = Instant.now();
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (var i = 0; i < 10_000; i++) {
+                executor.submit(task);
+            }
+        }
+        Duration ellapsedVirtualThreads  = Duration.between(init, Instant.now());
+
+        System.out.println("\nDone");
+        System.out.println("Platform threads took " + ellapsedPlatformThreads.toMillis() + " ms");
+        System.out.println("Virtual threads took " + ellapsedVirtualThreads.toMillis() + " ms");
+    }
+}
